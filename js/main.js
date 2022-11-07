b2fd2c86bd25d13e60c03b1a65d507c03947a97d
nasim-saaidi <302263973@student.rocmondriaan.nl>
2022-11-07 18:20:53 +0100

    bugged main.js

--- js/main.js
+++ js/main.js
@@ -18,15 +18,13 @@ function scrollingText(className, message, speed){
     //hoe lager het getal naast scrollingText, hoe sneller//
 }
 
-<<<<<<< HEAD
+
 scrollingText(".text", "Hello, you might not know who you are. But all you need to know is that you need to get out. There should be a key somewhere in this empty room. ", 75 );
-=======
-scrollingText(".text", `Hello, you might not know who you are.`, 75 );
 
 
- setTimeout(() => {
-     scrollingText(".text", "hahaha", 75 );
- }, 75*38);
 
 
->>>>>>> 6271ad16b844e9b42bb8318a36cde95e12a9740b
+
+
+
+
