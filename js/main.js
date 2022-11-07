a19909283ca18229b6252f68a71f65596fa9b3ae
nasim-saaidi <302263973@student.rocmondriaan.nl>
2022-11-07 13:20:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
+let done = false;
+
 function scrollingText(className, message, speed){
     let i = 0;
     let interval = setInterval(function(){
-        document.querySelector(className).innerHTML += message.charAt(i);
-        i++;
-        if (i > message.length){
-            clearInterval(interval);
+        if(done == false)
+        {
+            document.querySelector(className).innerHTML += message.charAt(i);
+            i++;
+            if (i > message.length){
+                clearInterval(interval);
+                done = true;
+                document.querySelector(className).innerHTML += "<br>";
+            }
+            done = false;
         }
     }, speed);
     //hoe lager het getal naast scrollingText, hoe sneller//
 }
 
-scrollingText(".text", "Hello, you might not know who you are. But all you need to know is that you need to get out. ", 75 );
+<<<<<<< HEAD
+scrollingText(".text", "Hello, you might not know who you are. But all you need to know is that you need to get out. There should be a key somewhere in this empty room. ", 75 );
+=======
+scrollingText(".text", `Hello, you might not know who you are.`, 75 );
+
+
+ setTimeout(() => {
+     scrollingText(".text", "hahaha", 75 );
+ }, 75*38);
+
+
+>>>>>>> 6271ad16b844e9b42bb8318a36cde95e12a9740b
