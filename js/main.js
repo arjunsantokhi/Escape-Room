21c1993cb4380ed8ccc693b7204708a8667380c1
arjunsantokhi <302338178@student.rocmondriaan.nl>
2022-11-07 13:15:37 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
+let done = false;
+
 function scrollingText(className, message, speed){
     let i = 0;
     let interval = setInterval(function(){
-        document.querySelector(className).innerHTML += message.charAt(i);
-        i++;
-        if (i > message.length){
-            clearInterval(interval);
+        if(done == false)
+        {
+            document.querySelector(className).innerHTML += message.charAt(i);
+            i++;
+            if (i > message.length){
+                clearInterval(interval);
+                done = true;
+                document.querySelector(className).innerHTML += "<br>";
+            }
+            done = false;
         }
     }, speed);
     //hoe lager het getal naast scrollingText, hoe sneller//
 }
 
-scrollingText(".text", "Hello, you might not know who you are. But all you need to know is that you need to get out. ", 75 );
+scrollingText(".text", `Hello, you might not know who you are.`, 75 );
+
+
+ setTimeout(() => {
+     scrollingText(".text", "hahaha", 75 );
+ }, 75*38);
+
+
