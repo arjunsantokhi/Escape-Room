7c4d07cd4d7a5aa9bec5ac13d872f6c0ffaf72e8
nasim-saaidi <302263973@student.rocmondriaan.nl>
2022-11-08 11:20:32 +0100

    vragen

--- js/main.js
+++ js/main.js
@@ -25,7 +25,6 @@ function scrollingText(className, message, speed){
 scrollingText(".text", "Hello, you might not know who you are. But all you need to know is that you need to get out. ", 75 );
 
 setTimeout(() => {
-
     scrollingText(".text", "There should be a key somewhere in this empty room.", 75 );
 }, 75*110);
 
@@ -34,8 +33,38 @@ setTimeout(function(){
 }, 75*175);
 
 setTimeout(() => {
-    scrollingText(".text", "There is a door in front of you, get out", 75 );
-}, 75*170);
+    scrollingText(".summaryText", "You wake up in an empty room", 75 );
+}, 75*200);
+
+setTimeout(() => {
+    scrollingText(".summaryText", "all alone. The only thing in front of you is a computer with a single application", 75 );
+}, 75*240);
+
+setTimeout(() => {
+    scrollingText(".summaryText", "you can see a key lying on the floor", 75 );
+}, 75*330);
+
+setTimeout(() => {
+    scrollingText(".choice-1-1", "> Walk to the door", 75 );
+}, 75*370);
+
+setTimeout(() => {
+    scrollingText(".choice-2-1", "> Grab the key", 75 );
+}, 75*400);
+
+setTimeout(() => {
+    scrollingText(".choice-3-1", '>   "What?" ', 75 );
+}, 75*430);
+
+
+
+
+
+
+
+
+
+
  
 
 
